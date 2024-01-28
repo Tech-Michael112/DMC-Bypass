d8b04fe9a60ffa1a664c42739f5b507eb49c7c58
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-01-28 11:41:38 +0100

    Update Bypass.py

--- Bypass.py
+++ Bypass.py
@@ -1,18 +1,5 @@
 import os, sys,time
-os.system('clear')
-print(""" 
-██████  ██    ██ ██████   █████  ███████ ███████     
-██   ██  ██  ██  ██   ██ ██   ██ ██      ██          
-██████    ████   ██████  ███████ ███████ ███████     
-██   ██    ██    ██      ██   ██      ██      ██     
-██████     ██    ██      ██   ██ ███████ ███████     
-[~] Bypassed By Michael
-[~] Run this tool to continue
-[~] Send key to Mr-Michael 
-[~] Bypass version - [1.0]
 
-""") 
-time.sleep(3)
 #os.system('xdg-open https://chat.whatsapp.com/GO3uvcJyyDpCQFHL48vlF3')
 try:
     __import__("HAX").menu()
