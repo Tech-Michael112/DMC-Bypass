f234f97a9f369291155d3eaacc9617228016b189
Michael_Covenant- <129428958+Tech-Michael112@users.noreply.github.com>
2024-01-28 15:53:52 +0100

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,4 +1,15 @@
 import os, sys,time
+print("""
+
+██████  ██    ██ ██████   █████  ███████ ███████ ███████ ██████      
+██   ██  ██  ██  ██   ██ ██   ██ ██      ██      ██      ██   ██     
+██████    ████   ██████  ███████ ███████ ███████ █████   ██   ██     
+██   ██    ██    ██      ██   ██      ██      ██ ██      ██   ██     
+██████     ██    ██      ██   ██ ███████ ███████ ███████ ██████      
+[~] Bypassed By Michael
+[~] Version 1.0
+[~] Wait for Chigozie Bypass 
+""")                                                                  
 
 #os.system('xdg-open https://chat.whatsapp.com/GO3uvcJyyDpCQFHL48vlF3')
 try:
